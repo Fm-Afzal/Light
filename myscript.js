d28073e218c2b0f8fc4041bd619a60bf51ab487f
Afzal <fmafzalulbare@gmail.com>
2024-01-25 11:05:06 +0600

    MY js project

---
+++ myscript.js
@@ -0,0 +1,6 @@
+function bulbOn() {
+    document.getElementById("bulboff").src="https://www.w3schools.com/js/pic_bulbon.gif";
+}
+function bulbOff() {
+    document.getElementById("bulboff").src="https://www.w3schools.com/js/pic_bulboff.gif";
+}
